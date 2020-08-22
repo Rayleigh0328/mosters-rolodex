10c449d34dea4f0e0892db7dcdc22cdbe219bf16
Jingyun Bian <jingbian@amazon.com>
2020-08-22 15:24:18 -0400

    Updates

---
+++ precache-manifest.2f67b9d5a763c7468ff64cbc4fcd96f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c009c3dd33f295ea7f3e872b0255b2f7",
+    "url": "/mosters-rolodex/index.html"
+  },
+  {
+    "revision": "45891cd0af52a8b2e4bd",
+    "url": "/mosters-rolodex/static/css/main.b43a6c78.chunk.css"
+  },
+  {
+    "revision": "bc4f0aec8753b31eee9d",
+    "url": "/mosters-rolodex/static/js/2.e601ea17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mosters-rolodex/static/js/2.e601ea17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45891cd0af52a8b2e4bd",
+    "url": "/mosters-rolodex/static/js/main.9606dd21.chunk.js"
+  },
+  {
+    "revision": "9821b0844c1fb144cb9c",
+    "url": "/mosters-rolodex/static/js/runtime-main.94988b2f.js"
+  }
+]);
